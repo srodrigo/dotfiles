9c6f63085aba463fa63957497546a0c4ea9ea40d
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-24 20:13:51 +0100

    [EMACS] add evil suspend emcas key binding

--- emacs/.emacs.d/lisp/init-vim.el
+++ emacs/.emacs.d/lisp/init-vim.el
@@ -16,7 +16,9 @@
     (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
     (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
     (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
-    (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right))
+    (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+
+    (define-key evil-motion-state-map (kbd "C-z") 'suspend-emacs))
 
 (use-package evil-surround
     :ensure t
