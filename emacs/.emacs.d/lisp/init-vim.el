47303c7afcf16238e13c7edea77cb90e835d7e03
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-24 18:50:33 +0100

    [EMACS] add vim chord

--- emacs/.emacs.d/lisp/init-vim.el
+++ emacs/.emacs.d/lisp/init-vim.el
@@ -1,6 +1,13 @@
+(use-package key-chord
+    :ensure t
+    :init)
+
 (use-package evil
     :ensure t
     :config
-    (evil-mode 1))
+    (evil-mode 1)
+    (setq key-chord-two-keys-delay 0.3)
+    (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+    (key-chord-mode 1))
 
 (provide 'init-vim)
