84a7a026b9159176e5261c6d5b01905d8d445285
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-24 21:24:49 +0100

    [EMACS] fix indentation

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,15 +1,15 @@
 (require 'package)
 (setq package-archives
-    '(("gnu" . "http://elpa.gnu.org/packages/")
-      ("marmalade" . "http://marmalade-repo.org/packages/")
-      ("melpa" . "http://melpa.org/packages/")))
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+    ("marmalade" . "http://marmalade-repo.org/packages/")
+    ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
-    (progn
-        (package-refresh-contents)
-        (package-install 'use-package)))
+  (progn
+    (package-refresh-contents)
+    (package-install 'use-package)))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
@@ -17,4 +17,4 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 
 (let ((file-name-handler-alist nil))
-    (require 'init-vim))
+  (require 'init-vim))
--- emacs/.emacs.d/lisp/init-vim.el
+++ emacs/.emacs.d/lisp/init-vim.el
@@ -1,39 +1,39 @@
 (use-package key-chord
-    :ensure t
-    :init)
+  :ensure t
+  :init)
 
 (use-package evil
-    :ensure t
-    :config
-    (evil-mode 1)
+  :ensure t
+  :config
+  (evil-mode 1)
 
-    ; Remap escape
-    (setq key-chord-two-keys-delay 0.3)
-    (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
-    (key-chord-mode 1)
+  ; Remap escape
+  (setq key-chord-two-keys-delay 0.3)
+  (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+  (key-chord-mode 1)
 
-    ; Navigate between windows
-    (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
-    (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
-    (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
-    (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+  ; Navigate between windows
+  (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
 
-    (define-key evil-motion-state-map (kbd "C-z") 'suspend-emacs))
+  (define-key evil-motion-state-map (kbd "C-z") 'suspend-emacs))
 
 (use-package evil-surround
-    :ensure t
-    :config
-    (global-evil-surround-mode 1))
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
 
 (use-package evil-leader
-    :ensure t
-    :config
-    (global-evil-leader-mode)
-    (evil-leader/set-leader ","))
+  :ensure t
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader ","))
 
 (use-package evil-nerd-commenter
-    :ensure t
-    :config
-    (evilnc-default-hotkeys))
+  :ensure t
+  :config
+  (evilnc-default-hotkeys))
 
 (provide 'init-vim)
