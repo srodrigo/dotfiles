5cf01b9a151c3a3c959382e805fcb7817c9db024
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-24 19:54:09 +0100

    [EMACS] evil remap navigate between windows

--- emacs/.emacs.d/lisp/init-vim.el
+++ emacs/.emacs.d/lisp/init-vim.el
@@ -6,8 +6,16 @@
     :ensure t
     :config
     (evil-mode 1)
+
+    ; Remap escape
     (setq key-chord-two-keys-delay 0.3)
     (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
-    (key-chord-mode 1))
+    (key-chord-mode 1)
+
+    ; Navigate between windows
+    (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+    (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+    (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+    (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right))
 
 (provide 'init-vim)
