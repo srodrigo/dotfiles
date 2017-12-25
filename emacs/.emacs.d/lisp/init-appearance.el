2be8f25177c7a8a6619298a2e26aec4bdebabd2c
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-25 12:41:48 +0100

    [EMACS] add line numbers (nlinum)

--- emacs/.emacs.d/lisp/init-appearance.el
+++ emacs/.emacs.d/lisp/init-appearance.el
@@ -13,4 +13,17 @@
   :config
   (load-theme 'airline-molokai))
 
+(use-package nlinum
+  :ensure t
+  :init
+  (add-hook 'nlinum-mode-hook #'my-nlinum-mode-hook))
+
+(defun my-nlinum-mode-hook ()
+  (when nlinum-mode
+    (setq-local nlinum-format
+      (concat "%" (number-to-string
+        ;; Guesstimate number of buffer lines.
+        (ceiling (log (max 1 (/ (buffer-size) 80)) 10)))
+        "d "))))
+
 (provide 'init-appearance)
