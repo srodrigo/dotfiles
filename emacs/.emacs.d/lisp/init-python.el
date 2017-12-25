058f75828d7c61e0220cbf61c9d96feeab0f4de7
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-25 22:32:01 +0100

    [EMACS] add elpy

--- emacs/.emacs.d/lisp/init-python.el
+++ emacs/.emacs.d/lisp/init-python.el
@@ -1,10 +1,21 @@
 (require 'programming-mode)
 
+(use-package elpy
+  :ensure t)
+
+(elpy-enable)
+
 (add-hook 'python-mode-hook 'programming-mode)
 
 (add-hook 'flycheck-mode-hook
   (lambda ()
     (progn
       (flycheck-add-mode 'python-pylint 'python-mode-hook))))
+
+(add-hook 'python-mode-hook
+  (lambda ()
+    (progn
+      (evil-leader/set-key "j" 'elpy-goto-definition)
+      (evil-leader/set-key "t" 'elpy-test))))
 
 (provide 'init-python)
