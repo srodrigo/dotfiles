ba350929eef33eccc5d9b49495795079ff3e4488
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-25 22:42:32 +0100

    [EMACS] navigate to next/prev flycheck error

--- emacs/.emacs.d/lisp/init-syntax-check.el
+++ emacs/.emacs.d/lisp/init-syntax-check.el
@@ -1,4 +1,8 @@
 (use-package flycheck
-  :ensure t)
+  :ensure t
+  :config
+
+  (evil-leader/set-key "n" 'flycheck-next-error)
+  (evil-leader/set-key "N" 'flycheck-previous-error))
 
 (provide 'init-syntax-check)
