93323fc9361a9f147dba849eee63bde4179e931b
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-26 19:13:23 +0100

    [EMACS] fix buffers list showing up

--- emacs/.emacs.d/lisp/init-file-finder.el
+++ emacs/.emacs.d/lisp/init-file-finder.el
@@ -6,7 +6,6 @@
   (global-set-key (kbd "C-p") 'helm-projectile-find-file)
   (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile-find-file)
 
-  (global-set-key (kbd "SPC") 'helm-mini)
   (define-key evil-normal-state-map (kbd "SPC") 'helm-mini))
 
 (provide 'init-file-finder)
