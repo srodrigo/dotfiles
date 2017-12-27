7214a55dc4f83ab0d2c07b89f30dae2ca96eec63
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-27 18:03:37 +0100

    [EMACS] add searching in project files

--- emacs/.emacs.d/lisp/init-file-finder.el
+++ emacs/.emacs.d/lisp/init-file-finder.el
@@ -1,5 +1,4 @@
-(use-package
-  helm-projectile
+(use-package helm-projectile
   :ensure t
   :config
 
@@ -7,5 +6,13 @@
   (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile-find-file)
 
   (define-key evil-normal-state-map (kbd "SPC") 'helm-mini))
+
+(use-package helm-ag
+  :ensure t
+  :config
+
+  (define-key evil-normal-state-map (kbd "C-f") 'helm-projectile-ag)
+  (customize-set-variable 'grep-find-ignored-directories
+    (list "SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "objects" "build" "bin" "out" "lib" "dist" "node_modules" ".nyc_output")))
 
 (provide 'init-file-finder)
