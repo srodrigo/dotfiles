7d39603fcf4326102aa11577f0a754935562c74f
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-27 22:41:31 +0100

    [EMACS] show flycheck errors in the correct buffers

--- emacs/.emacs.d/lisp/init-syntax-check.el
+++ emacs/.emacs.d/lisp/init-syntax-check.el
@@ -3,6 +3,9 @@
   :config
 
   (evil-leader/set-key "e" 'flycheck-next-error)
-  (evil-leader/set-key "E" 'flycheck-previous-error))
+  (evil-leader/set-key "E" 'flycheck-previous-error)
+
+  (display-in-buffer (rx bos "*Flycheck errors*"))
+  (display-in-buffer (rx bos "*Flycheck error messages*")))
 
 (provide 'init-syntax-check)
