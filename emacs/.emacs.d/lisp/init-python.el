f761dd465998c7c737aa5a6e611bde099a560d79
srodrigo <sergio.rodrigo.royo@gmail.com>
2017-12-29 15:25:53 +0100

    [EMACS] open Python shell with leader key

--- emacs/.emacs.d/lisp/init-python.el
+++ emacs/.emacs.d/lisp/init-python.el
@@ -18,6 +18,7 @@
   (lambda ()
     (progn
       (evil-leader/set-key "j" 'elpy-goto-definition)
-      (evil-leader/set-key "t" 'elpy-test))))
+      (evil-leader/set-key "t" 'elpy-test)
+      (evil-leader/set-key "s" 'elpy-shell-switch-to-shell))))
 
 (provide 'init-python)
