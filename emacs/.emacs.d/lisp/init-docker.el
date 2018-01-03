4c0a2d37509477cbed10e8ae323098c6a8d0a274
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-01-03 17:03:11 +0100

    [EMACS] add programming and gutter modes to dockerfile

--- emacs/.emacs.d/lisp/init-docker.el
+++ emacs/.emacs.d/lisp/init-docker.el
@@ -1,4 +1,10 @@
+(require 'programming-mode)
+(require 'git-gutter)
+
 (use-package dockerfile-mode
   :ensure t)
+
+(add-hook 'dockerfile-mode-hook 'programming-mode)
+(add-hook 'dockerfile-mode-hook 'gutter-mode)
 
 (provide 'init-docker)
