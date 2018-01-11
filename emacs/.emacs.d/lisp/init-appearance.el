07e6f13bae13f28f7c242c8d1ca8650722794d12
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-01-11 13:47:58 +0000

    [EMACs] fix compile mode colours

--- emacs/.emacs.d/lisp/init-appearance.el
+++ emacs/.emacs.d/lisp/init-appearance.el
@@ -37,6 +37,16 @@
 
 (menu-bar-mode -1)
 
+(use-package ansi-color
+  :ensure t)
+(defun endless/colorize-compilation ()
+  "Colorize from `compilation-filter-start' to `point'."
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region
+      compilation-filter-start (point))))
+
+(add-hook 'compilation-filter-hook 'endless/colorize-compilation)
+
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (provide 'init-appearance)
