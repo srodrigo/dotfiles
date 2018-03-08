f1a89aa4c10d527f0e98241a530cecade8c14fcf
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-08 10:14:34 +0000

    fix backup files

--- emacs/.emacs.d/lisp/backups.el
+++ emacs/.emacs.d/lisp/backups.el
@@ -1,9 +1,10 @@
 (setq
-   backup-by-copying t
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)
+  backup-by-copying t
+  create-lockfiles nil
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  make-backup-files t)
 
 (setq backup-directory-alist
   `((".*" . ,temporary-file-directory)))
