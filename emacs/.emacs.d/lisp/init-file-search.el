b2ed22626a956fc82a7069c0e5ab1fe34458bf01
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-03-08 10:15:49 +0000

    add Ack

--- emacs/.emacs.d/lisp/init-file-search.el
+++ emacs/.emacs.d/lisp/init-file-search.el
@@ -7,4 +7,16 @@
 			  (list "SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "objects" "build" "bin" "out" "lib" "dist" "node_modules" ".nyc_output"))
   (display-in-buffer (rx bos "*helm projectile*")))
 
+(evil-define-command evil-ack (arg)
+  (interactive "<a>")
+  (progn
+    (grep-compute-defaults)
+    (rgrep arg "*.*" (projectile-project-root)))) ; In linux (getenv "PWD") works
+
+(evil-ex-define-cmd "Ack" 'evil-ack)
+
+(customize-set-variable
+  'grep-find-ignored-directories
+  (list "SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "objects" "build" "bin" "out" "lib" "dist" "node_modules" ".nyc_output"))
+
 (provide 'init-file-search)
