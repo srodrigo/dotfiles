bf8ffd6cc96729ed7d0aa1476ecdf373c05bf848
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-04-17 18:23:50 +0100

    [EMACS] change key binding to escape from insert mode

--- emacs/.emacs.d/lisp/init-vim.el
+++ emacs/.emacs.d/lisp/init-vim.el
@@ -14,7 +14,7 @@
 
   ; Remap escape
   (setq key-chord-two-keys-delay 0.3)
-  (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
   (key-chord-mode 1)
 
   ; Navigate between windows
