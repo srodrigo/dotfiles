872f2a9dc00d72fe60c09b1d8bec3121105087ed
srodrigo <sergio.rodrigo.royo@gmail.com>
2018-04-17 18:22:57 +0100

    [EMACS] set smart parens for lisp

--- emacs/.emacs.d/lisp/init-el.el
+++ emacs/.emacs.d/lisp/init-el.el
@@ -1,9 +1,11 @@
 (require 'programming-mode)
 (require 'gutter-mode)
+(require 'init-parenthesis)
 
 (add-hook 'emacs-lisp-mode-hook 'programming-mode)
 (add-hook 'emacs-lisp-mode-hook 'gutter-mode)
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
 (add-hook 'flycheck-mode-hook
   (lambda () (
     add-to-list 'flycheck-disabled-checkers 'emacs-lisp-checkdoc)))
