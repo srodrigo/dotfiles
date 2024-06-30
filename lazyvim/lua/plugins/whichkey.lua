434d127b40c4a387cf13f7ecd38672dfc14a02c1
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-06-30 13:31:46 +0100

    fix(nvim): fix lazyvim showing window key options

--- lazyvim/lua/plugins/whichkey.lua
+++ lazyvim/lua/plugins/whichkey.lua
@@ -40,10 +40,5 @@ return {
         ["<leader>x"] = { name = "+diagnostics/quickfix" },
       },
     },
-    config = function(_, opts)
-      local wk = require("which-key")
-      wk.setup(opts)
-      wk.register(opts.defaults)
-    end,
   },
 }
