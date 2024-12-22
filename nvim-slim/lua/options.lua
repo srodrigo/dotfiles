4712bc3944de7d71a84145a249d32316314b1b90
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2024-12-22 16:54:04 +0000

    feat(nvim-slim): add keymap to escape highlighting

--- nvim-slim/lua/options.lua
+++ nvim-slim/lua/options.lua
@@ -4,3 +4,6 @@ vim.g.maplocalleader = " "
 
 -- Set if the terminal has a nerd font set up
 vim.g.have_nerd_font = true
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
